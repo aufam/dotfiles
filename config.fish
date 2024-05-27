9d57d725166f7a79c6d954d781405df9e088765e
aufa <60635730+aufam@users.noreply.github.com>
2024-05-27 18:41:16 +0700

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,10 @@
+# curl -fLo
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# sudo apt install bat exa
+alias cat batcat
+alias ls exa
+alias ll exa -l
