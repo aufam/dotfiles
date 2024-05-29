fb9c72701b730d1637030d43fcdb69cbc02d9ff3
aufa <60635730+aufam@users.noreply.github.com>
2024-05-29 11:49:10 +0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -6,5 +6,13 @@ end
 
 # sudo apt install bat exa
 alias cat batcat
-alias ls exa
-alias ll 'exa -l'
+alias ls 'exa --group-directories-first'
+alias la 'exa -a --group-directories-first'
+alias ll 'exa -l --group-directories-first'
+alias lt 'exa -T --group-directories-first'
+
+# bobthefish theme
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt "↪ " 
+set -g theme_date_format +%T 
