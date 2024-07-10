ab5387490ee796e45af95832f3781aa02bb374ec
aufa <60635730+aufam@users.noreply.github.com>
2024-07-10 11:47:02 +0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
-# curl -fLo ~/.config/fish/config.fish --create-dirs https://raw.githubusercontent.com/aufam/configs/main/config.fish
+# ~/.config/fish/config.fish
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -10,9 +10,56 @@ alias ls 'exa --group-directories-first'
 alias la 'exa -a --group-directories-first'
 alias ll 'exa -l --group-directories-first'
 alias lt 'exa -T --group-directories-first'
+alias please 'echo sudo $history[1] && eval command sudo $history[1]'
 
 # bobthefish theme
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_newline_cursor yes
-set -g theme_newline_prompt "↪ " 
-set -g theme_date_format +%T 
+set -g theme_nerd_fonts yes
+set -g theme_newline_prompt ">> "
+set -g theme_date_format +%T
+
+# extract
+function ex
+  if test (count $argv) -eq 0
+    echo "Usage: ex <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|exe|tar.bz2|tar.gz|tar.xz>"
+    echo "       extract <path/file_name_1.ext> [path/file_name_2.ext] [path/file_name_3.ext]"
+  else
+    for n in $argv
+      if test -f "$n"
+        switch "$n"
+          case '*.tar.bz2' '*.tar.gz' '*.tar.xz' '*.tbz2' '*.tgz' '*.txz' '*.tar'
+            tar xvf "$n"
+          case '*.lzma'
+            unlzma "$n"
+          case '*.bz2'
+            bunzip2 "$n"
+          case '*.rar' '*.cbr'
+            unrar x -ad "$n"
+          case '*.gz'
+            gunzip "$n"
+          case '*.zip' '*.cbz' '*.epub'
+            unzip "$n"
+          case '*.z'
+            uncompress "$n"
+          case '*.7z' '*.arj' '*.cab' '*.cb7' '*.chm' '*.deb' '*.dmg' '*.iso' '*.lzh' '*.msi' '*.pkg' '*.rpm' '*.udf' '*.wim' '*.xar'
+            7z x "$n"
+          case '*.xz'
+            unxz "$n"
+          case '*.exe'
+            cabextract "$n"
+          case '*.cpio'
+            cpio -id < "$n"
+          case '*.ace' '*.cba'
+            unace x "$n"
+          case '*'
+            echo "ex: '$n' - unknown archive method"
+            return 1
+        end
+      else
+        echo "'$n' - file does not exist"
+        return 1
+      end
+    end
+  end
+end
