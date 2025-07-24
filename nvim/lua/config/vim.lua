fcfb5ad577d85f6c2d42ab30b8736b2b248c7c71
aufam <aufamuhammad715@gmail.com>
2025-07-24 20:53:58 +0700

    some updates

--- nvim/lua/config/vim.lua
+++ nvim/lua/config/vim.lua
@@ -47,6 +47,25 @@ vim.cmd([[
   highlight default link TodoComment Todo
 ]])
 
+local version = vim.version()
+if version.major > 0 or version.minor >= 10 then
+	vim.diagnostic.config({
+		signs = {
+			text = {
+				[vim.diagnostic.severity.ERROR] = "",
+				[vim.diagnostic.severity.WARN] = "",
+				[vim.diagnostic.severity.INFO] = "",
+				[vim.diagnostic.severity.HINT] = "󰌵",
+			},
+		},
+	})
+else
+	vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+	vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+	vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+	vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+end
+
 -- Basic autocommands
 local augroup = vim.api.nvim_create_augroup("UserConfig", {})
 
