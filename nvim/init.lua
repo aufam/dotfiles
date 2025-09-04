c30f16ea6218ccf08b586e2b9396f18001d3cef1
aufam <aufamuhammad715@gmail.com>
2025-09-04 11:26:31 +0700

    change default colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -24,7 +24,7 @@ local no_lazy = vim.env.NO_LAZY
 
 if (version.major > 0 or version.minor >= 10) and not no_lazy then
 	require("config.lazy")
-	vim.cmd.colorscheme("melange")
+	vim.cmd.colorscheme("rose-pine")
 else
 	vim.notify(
 		"lazy.nvim and colorscheme disabled: " .. (no_lazy and "NO_LAZY env set" or "Neovim < 0.10"),
