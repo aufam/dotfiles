5614c480e4ca93021a9298dbd29babe140cfc2bc
aufam <aufa@comtrue.com>
2026-02-09 11:11:43 +0700

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -9,8 +9,10 @@ set -g fish_cursor_replace_one underscore
 set -g fish_cursor_visual      block
 
 set -x PATH $PATH /usr/local/go/bin $HOME/go/bin
+set -x PATH $PATH $HOME/.cargo/bin
 set -x CPM_SOURCE_CACHE $HOME/.cache/CPM
 set -x CPPXX_CACHE $HOME/.cache/cppxx
+set -x VIM_TRANSPARENT 1
 
 # aliases
 alias please 'echo sudo $history[1] && eval command sudo $history[1]'
@@ -50,7 +52,7 @@ end
 
 # gemini
 function gemini
-    argparse 'x/xclip' 'v/verbose' 'l/long' -- $argv
+    argparse 'f/file=' 'x/xclip' 'v/verbose' 'l/long' -- $argv
     or return
 
     if set -q _flag_long
@@ -59,10 +61,24 @@ function gemini
         set pre_prompt "answer shortly\n"
     end
 
+    set prompt "$pre_prompt$argv"
+    if test (count $argv) -eq 0; and not isatty stdin
+        while read line
+            set prompt "$prompt\n$line"
+        end
+    end
+
+    if set -q _flag_file
+        if test -f $_flag_file
+            set prompt "$prompt\n$(cat $_flag_file)"
+        else
+            echo "Error: File '$_flag_file' not found."
+            exit 1
+        end
+    end
+
     if set -q _flag_xclip
-        set prompt "$pre_prompt$argv\n$(xclip -selection clipboard -o)"
-    else
-        set prompt "$pre_prompt$argv"
+        set prompt "$prompt\n$(xclip -selection clipboard -o)"
     end
 
     set api_url "https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent"
@@ -79,47 +95,61 @@ function gemini
         | jq -r '.candidates[0].content.parts[0].text // .error.message'
 end
 
+function commit
+    set prompt "Write a concise git commit message in imperative mood. No quotes, no punctuation at the end."
+    set diff (git diff --staged --unified=0 | string collect)
+    set msg (printf "%s\n%s" "$prompt" "$diff" | gemini -l)
+
+    echo "message: $msg"
+    read -P "Commit with this message? [y/N] " confirm
+    if test "$confirm" != y
+        return 1
+    end
+
+    git commit -m "$msg"
+end
+
 # extract
 function ex
-  if test (count $argv) -eq 0
-    echo "Usage: ex <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|exe|tar.bz2|tar.gz|tar.xz>"
-    echo "       extract <path/file_name_1.ext> [path/file_name_2.ext] [path/file_name_3.ext]"
-  else
-    for n in $argv
-      if test -f "$n"
-        switch "$n"
-          case '*.tar.bz2' '*.tar.gz' '*.tar.xz' '*.tbz2' '*.tgz' '*.txz' '*.tar'
-            tar xvf "$n"
-          case '*.lzma'
-            unlzma "$n"
-          case '*.bz2'
-            bunzip2 "$n"
-          case '*.rar' '*.cbr'
-            unrar x -ad "$n"
-          case '*.gz'
-            gunzip "$n"
-          case '*.zip' '*.cbz' '*.epub'
-            unzip "$n"
-          case '*.z'
-            uncompress "$n"
-          case '*.7z' '*.arj' '*.cab' '*.cb7' '*.chm' '*.deb' '*.dmg' '*.iso' '*.lzh' '*.msi' '*.pkg' '*.rpm' '*.udf' '*.wim' '*.xar'
-            7z x "$n"
-          case '*.xz'
-            unxz "$n"
-          case '*.exe'
-            cabextract "$n"
-          case '*.cpio'
-            cpio -id < "$n"
-          case '*.ace' '*.cba'
-            unace x "$n"
-          case '*'
-            echo "ex: '$n' - unknown archive method"
-            return 1
+    if test (count $argv) -eq 0
+        echo "Usage: ex <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|exe|tar.bz2|tar.gz|tar.xz>"
+        echo "       extract <path/file_name_1.ext> [path/file_name_2.ext] [path/file_name_3.ext]"
+    else
+        for n in $argv
+            if test -f "$n"
+                switch "$n"
+                case '*.tar.bz2' '*.tar.gz' '*.tar.xz' '*.tbz2' '*.tgz' '*.txz' '*.tar'
+                    tar xvf "$n"
+                case '*.lzma'
+                    unlzma "$n"
+                case '*.bz2'
+                    bunzip2 "$n"
+                case '*.rar' '*.cbr'
+                    unrar x -ad "$n"
+                case '*.gz'
+                    gunzip "$n"
+                case '*.zip' '*.cbz' '*.epub'
+                    unzip "$n"
+                case '*.z'
+                    uncompress "$n"
+                case '*.7z' '*.arj' '*.cab' '*.cb7' '*.chm' '*.deb' '*.dmg' '*.iso' '*.lzh' '*.msi' '*.pkg' '*.rpm' '*.udf' '*.wim' '*.xar'
+                    7z x "$n"
+                case '*.xz'
+                    unxz "$n"
+                case '*.exe'
+                    cabextract "$n"
+                case '*.cpio'
+                    cpio -id < "$n"
+                case '*.ace' '*.cba'
+                    unace x "$n"
+                case '*'
+                    echo "ex: '$n' - unknown archive method"
+                    return 1
+                end
+            else
+                echo "'$n' - file does not exist"
+                return 1
+            end
         end
-      else
-        echo "'$n' - file does not exist"
-        return 1
-      end
     end
-  end
 end
