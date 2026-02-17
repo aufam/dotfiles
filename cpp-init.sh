1ea368da20a3e5b93024fc7aa4f1140d51741b95
aufam <aufamuhammad715@gmail.com>
2026-02-17 16:12:52 +0700

    update cpp-init

--- cpp-init.sh
+++ cpp-init.sh
@@ -11,31 +11,31 @@ PROJECT_DESCRIPTION=$3
 
 # Check if project name is provided
 if [ -z "$PROJECT_NAME" ]; then
-    echo "Usage: $0 <project_name> <project_version> <project_description>"
-    exit 1
+	echo "Usage: $0 <project_name> <project_version> <project_description>"
+	exit 1
 fi
 
 # Check if project version is provided
 if [ -z "$PROJECT_VERSION" ]; then
-    PROJECT_VERSION=0.1.0
+	PROJECT_VERSION=0.1.0
 fi
 
 # Create the project directory structure
-mkdir -p $PROJECT_NAME/src
-mkdir -p $PROJECT_NAME/cmake
+mkdir -p src
+mkdir -p cmake
 
 # Include CPM
 echo "Downloading CPM.cmake..."
-wget -q -O $PROJECT_NAME/cmake/CPM.cmake https://github.com/cpm-cmake/CPM.cmake/releases/latest/download/get_cpm.cmake
+wget -q -O cmake/CPM.cmake https://github.com/cpm-cmake/CPM.cmake/releases/latest/download/get_cpm.cmake
 
 # Check if CPM.cmake was successfully downloaded
 if [ $? -ne 0 ]; then
-    echo "Failed to download CPM.cmake. Check your internet connection."
-    exit 1
+	echo "Failed to download CPM.cmake."
+	exit 1
 fi
 
 # Create a basic main.cpp file
-cat <<EOL > $PROJECT_NAME/src/main.cpp
+cat <<EOL >src/main.cpp
 #include <fmt/format.h>
 
 int main() {
@@ -45,7 +45,7 @@ int main() {
 EOL
 
 # Create a basic CMakeLists.txt file
-cat <<EOL > $PROJECT_NAME/CMakeLists.txt
+cat <<EOL >CMakeLists.txt
 cmake_minimum_required(VERSION 3.14 FATAL_ERROR)
 
 # project settings
@@ -60,15 +60,89 @@ add_executable($PROJECT_NAME \${SOURCES})
 
 # external libraries
 include(cmake/CPM.cmake)
-CPMAddPackage("gh:aufam/delameta#main")
-CPMAddPackage("gh:catchorg/Catch2@3.7.0")
+
+## String formatter using fmtlib
+CPMAddPackage(
+    NAME            fmt
+    GIT_REPOSITORY  "https://github.com/fmtlib/fmt"
+    GIT_TAG         11.2.0
+    COMPONENTS      fmt
+)
+
+## Logging
+CPMAddPackage(
+    NAME            spdlog
+    GIT_REPOSITORY  "https://github.com/gabime/spdlog"
+    GIT_TAG         v1.15.3
+    OPTIONS         "SPDLOG_FMT_EXTERNAL ON"
+    COMPONENTS      spdlog
+)
+
+## JSON lib
+CPMAddPackage(
+    NAME            nlohmann_json
+    URL             "https://github.com/nlohmann/json/releases/download/v3.12.0/json.tar.xz"
+)
+
+## toml lib
+CPMAddPackage(
+    NAME            tomlplusplus
+    GIT_REPOSITORY  "https://github.com/marzer/tomlplusplus"
+    GIT_TAG         v3.4.0
+)
+
+## Boost's struct reflection
+CPMAddPackage(
+    NAME            pfr
+    GIT_REPOSITORY  "https://github.com/boostorg/pfr"
+    GIT_TAG         boost-1.88.0
+    OPTIONS         "BOOST_USE_MODULES OFF"
+    COMPONENTS      Boost::pfr
+)
+
+## Enum reflection
+CPMAddPackage(
+    NAME            magic_enum
+    GIT_REPOSITORY  "https://github.com/Neargye/magic_enum"
+    GIT_TAG         v0.9.7
+)
+
+## Option parser
+CPMAddPackage(
+    NAME            CLI11
+    GIT_REPOSITORY  "https://github.com/CLIUtils/CLI11"
+    GIT_TAG         v2.6.1
+)
+
+## Tests using GoogleTest
+CPMAddPackage(
+    NAME            googletest
+    GIT_REPOSITORY  "https://github.com/google/googletest"
+    GIT_TAG         v1.15.2
+    OPTIONS         "INSTALL_GTEST OFF"
+    COMPONENTS      gtest_main
+)
+
+## benchmark
+CPMAddPackage(
+    NAME            benchmark
+    GIT_REPOSITORY  "https://github.com/google/benchmark"
+    GIT_TAG         v1.9.2
+    OPTIONS         "BENCHMARK_ENABLE_INSTALL OFF" "BENCHMARK_INSTALL_DOCS OFF" "BENCHMARK_INSTALL_TOOLS OFF" "BENCHMARK_USE_BUNDLED_GTEST OFF"
+    COMPONENTS      benchmark_main
+)
 
 target_link_libraries($PROJECT_NAME PRIVATE
-    preprocessor
-    fmt-header-only
-    delameta
-    Catch2
+    fmt
+    spdlog
+    nlohmann_json
+    tomlplusplus::tomlplusplus
+    Boost::pfr
+    magic_enum
+    CLI11
 )
+
+target_compile_features($PROJECT_NAME PRIVATE cxx_std_17)
 
 # warnings
 target_compile_options($PROJECT_NAME PRIVATE
@@ -76,30 +150,13 @@ target_compile_options($PROJECT_NAME PRIVATE
     -Wextra
     -pedantic
 )
-
-# install
-install(
-    TARGETS     $PROJECT_NAME
-    DESTINATION \${CMAKE_INSTALL_BINDIR}
-)
-
-# cpack
-set(CPACK_PACKAGE_NAME $PROJECT_NAME)
-set(CPACK_PACKAGE_VERSION \${$(echo $PROJECT_NAME)_VERSION})
-set(CPACK_PACKAGE_DESCRIPTION_SUMMARY \${$(echo $PROJECT_NAME)_DESCRIPTIION})
-
-set(CPACK_DEBIAN_PACKAGE_MAINTAINER $(whoami))
-set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
-
-include(CPack)
-
 EOL
 
 # Create basic .gitgnore
-wget -q -O $PROJECT_NAME/.gitignore https://raw.githubusercontent.com/aufam/dotfiles/main/gitignore
+wget -q -O .gitignore https://raw.githubusercontent.com/aufam/dotfiles/main/gitignore
 
 # Init git
-cd $PROJECT_NAME && git init --initial-branch=main
+git init --initial-branch=main
 
 # Print success message
 echo "C++ project '$PROJECT_NAME' has been initialized."
